e45a21032dba827b0faa6505fb4e771c84ad8589
hyer0705 <hyer1205@gmail.com>
2024-05-21 00:31:25 +0900

    ✨feat: Input component 생성
    포켓몬 이름 검색창

--- practice-10-pokedex-app/src/App.jsx
+++ practice-10-pokedex-app/src/App.jsx
@@ -1,9 +1,13 @@
 import Header from "./components/Header";
+import Input from "./components/Input";
+import PokemonGallery from "./components/pokemon/PokemonGallery";
 
 function App() {
   return (
     <>
       <Header />
+      <Input />
+      <PokemonGallery />
     </>
   );
 }
